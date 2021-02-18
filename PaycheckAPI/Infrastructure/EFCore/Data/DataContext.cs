0ed725e146b7e91043b09a34502ac04491d2b427
Dilthey <diltheyaislan@gmail.com>
2021-02-17 23:29:20 -0300

    refactoring: infrastructure and EFCore layer

--- PaycheckAPI/Infrastructure/EFCore/Data/DataContext.cs
+++ PaycheckAPI/Infrastructure/EFCore/Data/DataContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using PaycheckAPI.Models;
+using PaycheckAPI.Entities;
 
-namespace PaycheckAPI.Data
+namespace PaycheckAPI.Infrastructure.EFCore.Data
 {
     public class DataContext : DbContext
     {
